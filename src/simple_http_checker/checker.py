8ff7d4e45cec04803c29c6d4b7bdb9c3caf429d3
Jose Segovia <jose.segovia@gmail.com>
2026-04-10 06:58:54 -0300

    ci(python-cicd): Test samantic release remove anused variable.

--- src/simple_http_checker/checker.py
+++ src/simple_http_checker/checker.py
@@ -44,5 +44,4 @@ def check_urls(urls: Collection[str], timeout: int = 5) -> dict[str, str]:
         results[url] = status
         logger.debug(f"Checked: {url:<40} {status}")
     logger.info("Check completed")
-    my_log = False
     return results
